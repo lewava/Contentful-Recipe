8ecdb898e3a7f0eb3cfb55e02278d3d26c7e9e07
lewava <leon.wass.vallin@gmail.com>
2021-12-09 14:17:00 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -12,27 +12,27 @@ export async function getStaticProps() {
   return {
     props: {
       recipes: res.items,
-      revalidate: 1,
     },
+    revalidate: 1,
   };
 }
 
 export default function Recipes({ recipes }) {
+  console.log(recipes);
+
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
         <RecipeCard key={recipe.sys.id} recipe={recipe} />
       ))}
 
-      <style jsx>
-        {`
-          .recipe-list {
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            grid-gap: 20px 60px;
-          }
-        `}
-      </style>
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
     </div>
   );
 }
